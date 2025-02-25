ad8303a27329686e9735c56e40e88351d8a75370
Alessandro Luongo <scinawa@luongo.pro>
2025-02-25 22:29:32 +0800

    Refactor visualization script for improved readability and formatting

--- scripts/visualize_training.py
+++ scripts/visualize_training.py
@@ -8,7 +8,11 @@ client = mlflow.tracking.MlflowClient()
 
 experiment = client.get_experiment_by_name("GaussGAN")
 print("Experiment ID:", experiment.experiment_id)
-runs = client.search_runs(experiment_ids=[experiment.experiment_id], order_by=["attribute.start_time desc"], max_results=1)
+runs = client.search_runs(
+    experiment_ids=[experiment.experiment_id],
+    order_by=["attribute.start_time desc"],
+    max_results=1,
+)
 
 print("Found", len(runs), "runs")
 run_id = runs[0].info.run_id
@@ -17,22 +21,23 @@ run_id = runs[0].info.run_id
 artifacts = client.list_artifacts(run_id)
 
 csv_artifacts = [a.path for a in artifacts if a.path.endswith(".csv")]
-csv_artifacts=csv_artifacts
+csv_artifacts = csv_artifacts
 
 print("Found CSV artifacts")
 
 fig_rows = (len(csv_artifacts) + 3) // 4
-fig, axes = plt.subplots(nrows=fig_rows, ncols=4, figsize=(12, 3*fig_rows))
-
+fig, axes = plt.subplots(nrows=fig_rows, ncols=4, figsize=(12, 3 * fig_rows))
 
 
 for idx, csv_path in enumerate(csv_artifacts):
     # print("Plotting artifact", csv_path)
     data_path = client.download_artifacts(run_id, csv_path)
     df = pd.read_csv(data_path, header=None, sep=",")
-    ax = axes[idx//4, idx%4]
+    ax = axes[idx // 4, idx % 4]
 
-    df = df.drop(0).apply(pd.to_numeric).round(3)  # Drop the first row and convert to numeric
+    df = (
+        df.drop(0).apply(pd.to_numeric).round(3)
+    )  # Drop the first row and convert to numeric
 
     # Convert to NumPy array
     data = df.values
@@ -41,12 +46,11 @@ for idx, csv_path in enumerate(csv_artifacts):
 
     ax.scatter(x_values, y_values, s=3)
 
-    ax.axhline(y=0, color='r', linestyle='--')
-    ax.axvline(x=0, color='r', linestyle='--')
+    ax.axhline(y=0, color="r", linestyle="--")
+    ax.axvline(x=0, color="r", linestyle="--")
 
     ax.set_xlim(-10, 10)
     ax.set_ylim(-10, 10)
-
 
     ax.set_title(f"Epoch {idx+1}")
 
@@ -57,4 +61,4 @@ print(f"Saving plot to {filename}")
 plt.savefig(filename)
 plt.savefig("training.png")
 plt.tight_layout()
-#plt.show()
+# plt.show()
